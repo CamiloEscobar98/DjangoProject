8316cda030c0c563a962e1e701c6460c57a9a540
Andrés Camilo Yáñez Escobar <camilo_escobar2398@outlook.com>
2023-06-19 00:18:41 -0500

    Finishing the test, but only basic steps

--- example/api/views.py
+++ example/api/views.py
@@ -1,50 +1,96 @@
 import requests
 
-from django.shortcuts import render
-
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import filters
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from example.models import Vulnerability
 from example.api.serializers import VulnerabilitySerializer
 
 class VulnerabilityApiViewSet(ModelViewSet):
+    queryset = Vulnerability.objects.all()     
     serializer_class = VulnerabilitySerializer
-    queryset = Vulnerability.objects.all()
 
-    def connectApi(self):
+    def connectApi(self, param = None):
         url = 'https://services.nvd.nist.gov/rest/json/cves/2.0'
-        response = requests.get(url, {})
+
+        if param is not None:
+            print(param)
+            response = requests.get(url, param)
+        else:
+            response = requests.get(url)            
 
         if response.status_code == 200:
             return response.json()
+        
+        return {}
+        
+    def getByVulnerabilityLevel(self, level = None):
+        arrayParameters = None
+
+        if level is not None:
+            strVulnerabilityLevel = str(level)
+            arrayParameters = {'cvssV3Severity' : strVulnerabilityLevel }
+        return self.connectApi(arrayParameters)
     
 
     def list(self, request, *args, **kwargs):
-
         response = self.connectApi()
-
         queryset = Vulnerability.objects.filter()
+
+        itemsVulnerabilities = []
 
         if response:
             itemsVulnerabilities = response.get('vulnerabilities')
             itemsFixed = list(queryset.values_list('code', flat=True))
-            itemsVulnerabilitiesFixed = [item for item in itemsVulnerabilities if item['cve']['id'] not in itemsFixed]
-        else:
-            itemsVulnerabilities = []
-
-        queryset = self.filter_queryset(self.get_queryset())
+            itemsVulnerabilitiesFixed = [
+                {
+                  'id' : item['cve']['id'],
+                  'sourceIdentifier' : item['cve']['sourceIdentifier'],  
+                  'published' : item['cve']['published'],
+                  'lastModified' : item['cve']['lastModified'],
+                  'descriptions' : item['cve']['descriptions'],
+                  'status' : item['cve']['vulnStatus']
+                }
+                for item in itemsVulnerabilities if item['cve']['id'] not in itemsFixed]
 
         response_data = {
-            'count' : len(itemsVulnerabilitiesFixed),
+            'count' : response.get('totalResults'),
             'results' : itemsVulnerabilitiesFixed
         } 
         return Response(response_data)
-
-    def aa():
-        return 1
     
+    @action(detail=False, methods=['GET'], url_path='severity', url_name='severity')
+    def getBySeverity(self, request):
+        severityLevel = None
+        levelParam = request.query_params.get('severity', None)
 
-    def getVulnerabilities(request):
-        return 1
-       
+        if levelParam is not None:
+            severityLevel = levelParam
+
+        print(severityLevel)
+
+        response = self.getByVulnerabilityLevel(severityLevel)
+
+        items = response.get('vulnerabilities')
+
+        itemsByVulnerability = []
+
+        if items:
+            itemsByVulnerability = [
+                 {
+                  'id' : item['cve']['id'],
+                  'sourceIdentifier' : item['cve']['sourceIdentifier'],  
+                  'published' : item['cve']['published'],
+                  'lastModified' : item['cve']['lastModified'],
+                  'descriptions' : item['cve']['descriptions'],
+                  'status' : item['cve']['vulnStatus']
+                }
+                 for item in items]
+        response_data = {
+            'count' : response.get('totalResults'),
+            'results' : itemsByVulnerability
+        }
+
+        return Response(response_data)
